0a9715483aa2c69c279adf52042e401e1276aaa2
alifprihantoro <alifprihantoro@gmail.com>
2023-08-25 08:22:13 +0700

    add explain and test

--- 27. Remove Element/index.test.ts
+++ 27. Remove Element/index.test.ts
@@ -3,12 +3,20 @@ import mvEl from './'
 test('example 1', () => {
   const nums = [3, 2, 2, 3]
   const val = 3
-  mvEl(nums, val)
-
+  const result = mvEl(nums, val)
+  result.NUMS.forEach((v, i) => {
+    if (i <= result.k) {
+      expect(v).not.toBe(val)
+    }
+  })
 })
 test('example 2', () => {
   const nums = [0, 1, 2, 2, 3, 0, 4, 2]
   const val = 2
-  mvEl(nums, val)
-
+  const result = mvEl(nums, val)
+  result.NUMS.forEach((v, i) => {
+    if (i <= result.k) {
+      expect(v).not.toBe(val)
+    }
+  })
 })
--- 27. Remove Element/index.ts
+++ 27. Remove Element/index.ts
@@ -1,3 +1,10 @@
+let NUMS: number[]
+// explain :
+// mengganti nums array yang memiliki nilai sama dengan val
+// nums array tidak harus urut
+// tidak menggunakan tambahan variable (langsung di nums arg)
+// misal nums = [0, 1, 2, 2, 3, 0, 4, 2] val=2 => nums = [ 0, 1, 3, 0, 4, 0, 4, 2 ] return 5
+// maka akan diambil angka 5 angka pertama dari return function
 function removeElement(nums: number[], val: number): number {
   // Counter for keeping track of elements other than val
   let k = 0;
@@ -8,6 +15,13 @@ function removeElement(nums: number[], val: number): number {
       nums[k++] = nums[i];
     }
   }
+  NUMS = nums
+
   return k;
 };
-export default removeElement
+
+function result(nums: number[], val: number): { NUMS: number[], k: number } {
+  const k = removeElement(nums, val)
+  return { NUMS, k }
+}
+export default result
