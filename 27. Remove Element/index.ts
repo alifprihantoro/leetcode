5d5496ab7be9aa2e2820ea85232d97c67d99f0a4
alifprihantoro <alifprihantoro@gmail.com>
2023-08-25 07:27:11 +0700

    del var

--- 27. Remove Element/index.ts
+++ 27. Remove Element/index.ts
@@ -3,10 +3,9 @@ function removeElement(nums: number[], val: number): number {
   let k = 0;
   // Loop through all the elements of the array
   for (let i = 0; i < nums.length; i++) {
-    const v = nums[i]
     // If the element is not val
-    if (v !== val) {
-      nums[k++] = v;
+    if (nums[i] !== val) {
+      nums[k++] = nums[i];
     }
   }
   return k;
